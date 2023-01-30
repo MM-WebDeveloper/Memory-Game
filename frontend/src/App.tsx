f57bb4f27e1998bc1f6b3ac700e50b925faf61c3
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-01-30 19:31:21 +0100

    feat: :sparkles: Add card shuffling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,36 @@
-import './App.css';
+import { useState } from 'react';
 
-function App() {
-	return (
-		<div className='App'>
-			<h1>Hello from App</h1>
-		</div>
-	);
+const cardImages = [
+	{ src: '/img/html-logo' },
+	{ src: '/img/css-logo' },
+	{ src: '/img/js-logo' },
+	{ src: '/img/react-logo' },
+	{ src: '/img/sass-logo' },
+	{ src: '/img/react-logo' },
+];
+
+interface Card {
+	id: number;
+	src: string;
 }
 
+const App = (): JSX.Element => {
+	const [cards, setCards] = useState<Card[]>([]);
+	// shuffle cards
+	const shuffleCards = () => {
+		const shuffledCards = [...cardImages, ...cardImages]
+			.sort(() => Math.random() - 0.5)
+			.map((card) => ({ ...card, id: Math.random() }));
+
+		console.log(shuffledCards);
+		setCards(shuffledCards);
+	};
+
+	return (
+		<div>
+			<h1>Memory Game</h1>
+			<button onClick={shuffleCards}>New Game</button>
+		</div>
+	);
+};
 export default App;
