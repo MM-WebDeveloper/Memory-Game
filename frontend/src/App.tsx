49377c8b84a5239fcd217d883595f331670f7240
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-02-03 19:21:45 +0100

    refactor: :recycle: Move cards data to its own ts file and import to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.scss';
 import Card from './components/Card';
-const cardImages = [
-	{ color: '#000', name: 'Black', matched: false },
-	{ color: '#aec6d2', name: 'Sonic Blue', matched: false },
-	{ color: '#ff0800', name: 'Candy Apple Red', matched: false },
-	{ color: '#f1eee6', name: 'Olympic White', matched: false },
-	{ color: '#3f5c94', name: 'Lake Placid Blue', matched: false },
-	{ color: '#f3dde7', name: 'Mary Kaye White', matched: false },
-];
+import { cardImages } from './cardsData';
 
 interface ICard {
 	id: number;
---
+++ frontend/src/cardsData.ts
@@ -0,0 +1,8 @@
+export const cardImages = [
+	{ color: '#000', name: 'Black', matched: false },
+	{ color: '#aec6d2', name: 'Sonic Blue', matched: false },
+	{ color: '#ff0800', name: 'Candy Apple Red', matched: false },
+	{ color: '#f1eee6', name: 'Olympic White', matched: false },
+	{ color: '#3f5c94', name: 'Lake Placid Blue', matched: false },
+	{ color: '#f3dde7', name: 'Mary Kaye White', matched: false },
+];
